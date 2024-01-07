d6c2f3bd294b9b443a76df4c2d48bbab148b9274
Colin Bagley <cjbagley@googlemail.com>
2024-01-07 14:29:38 +0000

    Added failing tests, ready for functionality

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+
+def get_url_end_location(url = ''):
+    return url
+
+def main() -> int:
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ test_main.py
@@ -0,0 +1,16 @@
+import unittest
+
+from main import get_url_end_location
+
+class TestFindUrlEndpoint(unittest.TestCase):
+    def test_find_correct_url_endpoint(self):
+        endpoint = 'http://bbc.co.uk'
+        expected = 'http://www.bbc.co.uk'
+        result = get_url_end_location(endpoint)
+
+        self.assertEqual(result, expected)
+
+    def test_incorrect_url_given(self):
+        endpoint = 'bbc.co.uk'
+
+        self.assertRaises(ValueError, get_url_end_location, endpoint)
