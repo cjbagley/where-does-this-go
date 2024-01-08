02331dd33706d2bc2fc5383e5e7cf9efe94a71fa
Colin Bagley <cjbagley@googlemail.com>
2024-01-08 11:15:52 +0000

    Basic functionality before tidying up

--- src/wdtg/wdtg.py
+++ src/wdtg/wdtg.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
 import sys
 import requests
+import argparse
+import urllib.parse
 
 
 def get_url_end_location(url=''):
-    r = requests.head(url=url, allow_redirects=True)
-    return url
+    return requests.head(url=url, allow_redirects=True).url
+
+
+def sanitise_url(url) -> str:
+    trim_after = [';', '`', '*', '!']
+    trimmed_url = url
+    for i, char in enumerate(url):
+        if char in trim_after:
+            trimmed_url = trimmed_url[:i]
+            break
+    return urllib.parse.quote(trimmed_url, safe='/:?&')
+
+
+def is_valid_url(url) -> bool:
+    try:
+        parsed = urllib.parse.urlparse(url)
+        return all([parsed.scheme, parsed.netloc])
+    except Exception:
+        return False
+
+
+def __get_args():
+    parser = argparse.ArgumentParser(
+        prog="Where Does This Go?",
+        description="Find out where a given url redirects to",
+    )
+
+    parser.add_argument(
+        'url',
+        help='The URL to follow',
+    )
+    return parser.parse_args()
 
 
 def main() -> int:
@@ -13,4 +45,7 @@ def main() -> int:
 
 
 if __name__ == '__main__':
+    args = __get_args()
+    result = get_url_end_location(args.url)
+    print(result)
     sys.exit(main())
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,36 @@
 import unittest
+from src.wdtg.wdtg import get_url_end_location, is_valid_url, sanitise_url
 
-from src.wdtg.wdtg import get_url_end_location
 
-
-class TestFindUrlEndpoint(unittest.TestCase):
+class TestMain(unittest.TestCase):
     def test_find_correct_url_endpoint(self):
-        endpoint = 'http://bbc.co.uk'
-        expected = 'http://www.bbc.co.uk'
+        endpoint = 'https://bbc.co.uk'
+        expected = 'https://www.bbc.co.uk'
         result = get_url_end_location(endpoint)
-
         self.assertEqual(result, expected)
 
     def test_incorrect_url_given(self):
         endpoint = 'bbc.co.uk'
-
         self.assertRaises(ValueError, get_url_end_location, endpoint)
+
+    def test_sanitise_url(self):
+        value_1 = 'https://bbc.co.uk'
+        expected_1 = 'https://bbc.co.uk'
+        result_1 = sanitise_url(value_1)
+        self.assertEqual(result_1, expected_1)
+
+        value_2 = 'https://bbc.co.uk?test=2&3 \';`; DROP *'
+        expected_2 = 'https://bbc.co.uk?test%3D2&3%20%27'
+        result_2 = sanitise_url(value_2)
+        self.assertEqual(result_2, expected_2)
+
+    def test_is_valid_url(self):
+        value_1 = 'https://bbc.co.uk'
+        expected_1 = True
+        result_1 = is_valid_url(value_1)
+        self.assertEqual(expected_1, result_1)
+
+        value_2 = 'bbc.co.uk'
+        expected_2 = False
+        result_2 = is_valid_url(value_2)
+        self.assertEqual(expected_2, result_2)
